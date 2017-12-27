c0b3064cfaeb57cf06032df1fbfdb9cd24c34d22
Maetad Sukarasud <maetad91@gmail.com>
2017-12-27 22:43:43 +0700

    Todo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import List from './List.jsx'
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = {
+      term: '',
+      items: []
+    }
+  }
+
+  onChange (event) {
+    this.setState({ term: event.target.value })
+  }
+
+  onSubmit (event) {
+    event.preventDefault()
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    })
+  }
+
   render () {
-    return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello world</h1>
-      </div>)
+    return (<div>
+      <form className="App" onSubmit={this.onSubmit}>
+        <input value={this.state.term} onChange={this.onChange} />
+        <button>Submit</button>
+      </form>
+      <List items={this.state.items} />
+    </div>)
   }
 }
